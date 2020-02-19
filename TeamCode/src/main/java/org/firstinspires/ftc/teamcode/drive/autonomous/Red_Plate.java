8844c5abd72e557748ab891be9fa80d0c874c7c7
Tron404 <edi.raul@yahoo.com>
2020-02-19 13:38:54 +0200

    Full Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/autonomous/Red_Plate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/autonomous/Red_Plate.java
@@ -10,8 +10,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.drive.mecanum.SampleMecanumDriveBase;
 import org.firstinspires.ftc.teamcode.drive.mecanum.SampleMecanumDriveREV;
 
-@Autonomous(name="Blue_Plate", group = "BLUE")
-public class Blue_Plate extends LinearOpMode {
+@Autonomous(name="Red_Plate", group = "RED")
+public class Red_Plate extends LinearOpMode {
 
     @Override
     public void runOpMode(){
@@ -19,7 +19,7 @@ public class Blue_Plate extends LinearOpMode {
         autoGripper auto = new autoGripper();
         auto.init(hardwareMap);
         SampleMecanumDriveBase drive = new SampleMecanumDriveREV(hardwareMap);
-        drive.setPoseEstimate(new Pose2d(42.0, 72.0, Math.toRadians(270.0)));
+        drive.setPoseEstimate(new Pose2d(42.0, -72.0, Math.toRadians(90.0)));
         //  drive.setLocalizer(new MecanumDrive.MecanumLocalizer(drive, true));
 
         waitForStart();
@@ -28,31 +28,43 @@ public class Blue_Plate extends LinearOpMode {
 
         if (isStopRequested()) return;
 
-            drive.followTrajectorySync(
-                    drive.trajectoryBuilder()
-                            .setReversed(false)
-                            .splineTo(new Pose2d(46,28, Math.toRadians(270)), new SplineInterpolator(Math.toRadians(270),Math.toRadians(95)))
-                            .build()
-            );
+        drive.followTrajectorySync(
+                drive.trajectoryBuilder()
+                        .setReversed(false)
+                        .splineTo(new Pose2d(45,-27, Math.toRadians(180)), new SplineInterpolator(Math.toRadians(90),Math.toRadians(260)))
+                        .build()
+        );
+
+        drive.followTrajectorySync(
+                drive.trajectoryBuilder()
+                .back(5)
+                .build()
+        );
 
         auto.ClawDown(hardwareMap);
         sleep(1000);
 
-            drive.turnSync(Math.toRadians(90));
-
-            drive.followTrajectorySync(
-                    drive.trajectoryBuilder()
-                    .strafeRight(55.0)
-                    .build()
-            );
-
-            drive.turnSync(Math.toRadians(7));
-
-            auto.ClawUp(hardwareMap);
+        drive.turnSync(Math.toRadians(-90));
 
         drive.followTrajectorySync(
                 drive.trajectoryBuilder()
-                        .strafeRight(20.0)
+                .forward(5)
+                .build()
+        );
+
+        drive.followTrajectorySync(
+                drive.trajectoryBuilder()
+                        .strafeLeft(60.0)
+                        .build()
+        );
+
+        drive.turnSync(Math.toRadians(-7));
+
+        auto.ClawUp(hardwareMap);
+
+        drive.followTrajectorySync(
+                drive.trajectoryBuilder()
+                        .strafeLeft(20.0)
                         .build()
         );
 
